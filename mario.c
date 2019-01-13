b3a1f2cc26f89f3207a0236301a679c585796526
zeyadsaleh <zeyadsaleh@users.noreply.github.com>
2019-01-13 00:10:30 +0000

    20190113T000915Z

--- mario.c
+++ mario.c
@@ -3,20 +3,20 @@
 
 int main(void)
 {
-int n;
+    int n;
     do // prompt the user for input
     {
-        n=get_int("Hight= "); // chech the value is valid
+        n = get_int("Hight= "); // chech the value is valid
     }
-    while (n<1 || n>8);
+    while (n < 1 || n > 8);
     
-    for (int i=0; i<n; i++) // printing new line
+    for (int i = 0; i < n; i++) // printing new line
     {
-        for (int j = 0; j<n-(i+1); j++) //printing the spaces 
+        for (int j = 0; j < n - (i + 1); j++) //printing the spaces 
         {
             printf(" ");
         }
-        for (int x = 0; x<i+1; x++) //printing the hashes
+        for (int x = 0; x < i + 1; x++) //printing the hashes
         {
             printf("#");
         }
